66aac60d21676a4788d7303eb614aab21bc435a2
imperadordf <imperadordf@users.noreply.github.com>
2020-08-17 22:43:26 -0300

    Player

--- Assets/Assets/Scripts/Player.cs
+++ Assets/Assets/Scripts/Player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Animator animator_Player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator_Player = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
         
+        animator_Player.SetFloat("X", x);
+        animator_Player.SetFloat("Y", y);
+
+        animator_Player.SetBool("IsSpriting",Input.GetKey(KeyCode.LeftShift));
     }
 }
