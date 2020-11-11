cec543d6fed0aca8eb3bbb7e0dae5e957c00cc2f
imperadordf <imperadordf@users.noreply.github.com>
2020-11-11 16:48:42 -0300

    Som

---
+++ Assets/Assets/Scripts/SomManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomManager : MonoBehaviour
+{
+    public static SomManager instancie;
+    public AudioSource audioManager;
+    private void Awake()
+    {
+        if (!instancie)
+        {
+            instancie = this;
+
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+
+    public void TocarSom(AudioClip som)
+    {
+        audioManager.PlayOneShot(som);
+    }
+}
