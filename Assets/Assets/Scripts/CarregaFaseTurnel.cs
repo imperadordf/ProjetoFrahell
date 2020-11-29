9216d1ad6a6ebc8ecd3082b1b74ad254230ed30c
imperadordf <imperadordf@users.noreply.github.com>
2020-11-28 23:34:19 -0300

    Tunel 2

--- Assets/Assets/Scripts/CarregaFaseTurnel.cs
+++ Assets/Assets/Scripts/CarregaFaseTurnel.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
 
 public class CarregaFaseTurnel : MonoBehaviour
 {
     public string NomeFase;
     public bool isback;
+   public PlayableDirector timeline;
+    
     public void CarregaFase()
+    {
+        timeline.Play();
+        GameManager.instancie.carregandoFase = true;
+        Invoke("CarregarCena",(float)timeline.duration - 1);
+    }
+
+
+    public void CarregarCena()
     {
         ManageFase.instancie.voltando = isback;
         SceneManager.LoadScene(NomeFase);
