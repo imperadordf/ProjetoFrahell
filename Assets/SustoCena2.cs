e047b021a7c4e2c7ced25ed077270127cc3bcc71
imperadordf <imperadordf@users.noreply.github.com>
2020-11-28 21:45:39 -0300

    Cena 2 Susto programado (falta uma parte)

---
+++ Assets/SustoCena2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class SustoCena2 : MonoBehaviour
+{
+    public PlayableDirector timelineSusto;
+    public GameObject inimigoSusto;
+    public MouseLook mouse;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            mouse.enabled = false;
+            timelineSusto.Play();
+            Invoke("LigaMouseLook",(float)timelineSusto.duration);
+            this.gameObject.SetActive(false);
+            
+        }
+    }
+   
+    private void LigaMouseLook()
+    {
+        mouse.enabled = true;
+    }
+}
