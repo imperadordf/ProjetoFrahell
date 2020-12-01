e89847a28b3e4300fc840e3c1301a68f0bcc8104
imperadordf <imperadordf@users.noreply.github.com>
2020-12-01 13:23:48 -0300

    Legenda na primeira fase

---
+++ Assets/Assets/Scripts/Legenda/LegendaBasica.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using TMPro;
+
+public class LegendaBasica : PlayableBehaviour
+{
+    public string legendaTexto;
+
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+        TextMeshProUGUI text = playerData as TextMeshProUGUI;
+        text.text = legendaTexto;
+        text.color = new Color(1, 1, 1, info.weight);
+    }
+}
---
+++ Assets/Assets/Scripts/Legenda/LegendaClip.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class LegendaClip : PlayableAsset
+{
+    public string legendaText;
+
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+    {
+        var playable = ScriptPlayable<LegendaBasica>.Create(graph);
+
+        LegendaBasica legendabasica = playable.GetBehaviour();
+        legendabasica.legendaTexto = legendaText;
+
+        return playable;
+    }
+}
